f2b64343e5263ae126ebe309abad31329740ff93
Enock Simiyu <60577078+enock295simiyu@users.noreply.github.com>
2022-02-22 22:53:25 +0300

    Change font size on home page

--- authentication.py
+++ authentication.py
@@ -1,8 +1,22 @@
-from tkinter import Tk, Label, Button, Toplevel, StringVar, Entry, END
+import webbrowser
+from tkinter import Tk, Label, Button, Toplevel, StringVar, Entry, END, Scale, HORIZONTAL
+
+from database import User
 
 
 class Authentication:
     def __init__(self):
+        self.material_title = None
+        self.link5 = None
+        self.link4 = None
+        self.link3 = None
+        self.link2 = None
+        self.link1 = None
+        self.font_title = None
+        self.user_failed_to_login_screen = None
+        self.user_logged_in_successfully_screen = None
+        self.user_created_successfully_screen = None
+        self.user_already_saved_screen = None
         self.password_login_entry = None
         self.username_login_entry = None
         self.password_verify = None
@@ -16,6 +30,7 @@ class Authentication:
         self.password1 = None
         self.username_entry = None
         self.password_entry = None
+        self.user = User(self.username, self.password)
 
     def create_base_authentication_window(self):
         self.authentication_window.title('Authentication System | Please Choose between login or register')
@@ -55,23 +70,36 @@ class Authentication:
         username_info = self.username.get()
         password_info = self.password.get()
         password_info1 = self.password1.get()
+        self.user.username = username_info
+        self.user.password = password_info1
         if password_info1 != password_info:
             pass
-
-        # Open file in write mode
-        file = open(username_info, "w")
-
-        # write username and password information into file
-        file.write(username_info + "\n")
-        file.write(password_info)
-        file.close()
-
+        user_created, self.user = self.user.save()
         self.username_entry.delete(0, END)
         self.password_entry.delete(0, END)
+        if not user_created:
+            self.user_already_saved()
+        else:
+            self.user_created_successfully()
 
-        # set a label for showing success information on screen
+        self.registration_screen.destroy()
 
-        Label(self.registration_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+    def user_already_saved(self):
+        self.user_already_saved_screen = Toplevel(self.registration_screen)
+        self.user_already_saved_screen.title("The Username you entered is already saved in the database.")
+        self.user_already_saved_screen.geometry("800x100")
+        Label(self.user_already_saved_screen, text="The Username you entered is already saved in the database. "
+                                                   "Please Login or create a new account!").pack()
+        Button(self.user_already_saved_screen, text="Login", command=self.login_page).pack()
+        Button(self.user_already_saved_screen, text="Register", command=self.register_page).pack()
+
+    def user_created_successfully(self):
+        self.user_created_successfully_screen = Toplevel(self.registration_screen)
+        self.user_created_successfully_screen.title("The Username account created Successfully.")
+        self.user_created_successfully_screen.geometry("800x100")
+        Label(self.user_created_successfully_screen, text="The Username account created Successfully. Please Login "
+              ).pack()
+        Button(self.user_created_successfully_screen, text="Login", command=self.login_page).pack()
 
     def login_page(self):
         self.login_screen = Toplevel(self.authentication_window)
@@ -92,10 +120,85 @@ class Authentication:
         Label(self.login_screen, text="").pack()
         Button(self.login_screen, text="Login", width=10, height=1, command=self.login_user).pack()
 
+    def user_logged_in_successfully(self):
+        self.user_logged_in_successfully_screen = Toplevel(self.login_screen)
+        self.user_logged_in_successfully_screen.title("Login is successful.")
+        self.user_logged_in_successfully_screen.geometry("800x100")
+        Label(self.user_logged_in_successfully_screen, text="Click the button below to view the main content page"
+              ).pack()
+        Button(self.user_created_successfully_screen, text="Main Page", ).pack()
+
+    def user_failed_to_login(self):
+        self.user_failed_to_login_screen = Toplevel(self.registration_screen)
+        self.user_failed_to_login_screen.title("Invalid Credentials.")
+        self.user_failed_to_login_screen.geometry("800x100")
+        Label(self.user_failed_to_login_screen, text="Either the username you entered or password is incorrect!"
+              ).pack()
+        Button(self.user_failed_to_login_screen, text="Login", command=self.login_page).pack()
+
     def login_user(self):
         self.username = self.username_verify.get()
         self.password = self.password_verify.get()
+        self.user.username = self.username
+        self.user.password = self.password
+        user_available = self.user.check_if_user_is_already_saved()
+        has_login = False
+        if user_available:
+            if user_available.get('password') == self.password:
+                self.user_logged_in_successfully()
+                has_login = True
+        if not has_login:
+            self.user_failed_to_login()
+
         self.username_login_entry.delete(0, END)
         self.password_login_entry.delete(0, END)
         Label(self.login_screen, text="Login was Success", fg="green", font=("calibri", 11)).pack()
         self.login_screen.destroy()
+        self.clear_main_page()
+
+    def callback(self, url):
+        webbrowser.open_new(url)
+
+    def change_font_size(self, val):
+        self.font_title.configure(font=('Calibri', val))
+        self.link2.configure(font=('Calibri', val))
+        self.link1.configure(font=('Calibri', val))
+        self.link3.configure(font=('Calibri', val))
+        self.link4.configure(font=('Calibri', val))
+        self.link5.configure(font=('Calibri', val))
+        self.material_title.configure(font=('Calibri', val))
+
+    def clear_main_page(self):
+        elements = self.authentication_window.pack_slaves()
+        for l in elements:
+            l.destroy()
+        self.authentication_window.title('Main Content')
+        self.font_title = Label(text="Change Font Size", bg="blue", fg='white', width="300", height="2",
+                                font=("Calibri", 13))
+        self.font_title.pack()
+        Label(text="").pack()
+        scale = Scale(self.authentication_window, orient=HORIZONTAL, length=300, width=20, sliderlength=10, from_=10,
+                      to=50,
+                      tickinterval=5, command=self.change_font_size)
+        scale.pack()
+        self.material_title = Label(text="List of the best python learning websites", bg="blue", fg='white',
+                                    width="300", height="2", font=("Calibri", 13))
+        self.material_title.pack()
+        Label(text="").pack()
+        self.link1 = Label(self.authentication_window, text="Google", fg="blue", font=("Calibri", 13), cursor="hand2")
+        self.link1.pack()
+        self.link1.bind("<Button-1>", lambda e: self.callback("https://www.google.com"))
+        self.link2 = Label(self.authentication_window, text="Udemy", fg="blue", font=("Calibri", 13), cursor="hand2")
+        self.link2.pack()
+        self.link2.bind("<Button-1>", lambda e: self.callback("https://www.udemy.com/"))
+        self.link3 = Label(self.authentication_window, text="CodeCademy", fg="blue", font=("Calibri", 13),
+                           cursor="hand2")
+        self.link3.pack()
+        self.link3.bind("<Button-1>", lambda e: self.callback("https://www.codecademy.com/"))
+        self.link4 = Label(self.authentication_window, text="Educative", fg="blue", font=("Calibri", 13),
+                           cursor="hand2")
+        self.link4.pack()
+        self.link4.bind("<Button-1>", lambda e: self.callback("https://www.educative.io/"))
+        self.link5 = Label(self.authentication_window, text="Coursera", fg="blue", font=("Calibri", 13), cursor="hand2")
+        self.link5.pack()
+        self.link5.bind("<Button-1>", lambda e: self.callback("https://www.coursera.org/"))
---
+++ database.py
@@ -0,0 +1,51 @@
+import csv
+
+import pandas as pd
+
+
+class User:
+    def __init__(self, username, password):
+        """
+        This function initializes a new user object
+        :param username: The username value of the user
+        :param password: The password value of the user
+        """
+        self.authentication_csv_file_location = 'database.csv'
+        self.username = username
+        self.password = password
+        self.is_authenticated = False
+
+    def check_if_user_is_already_saved(self):
+        """
+        This function check if a user is already saved in the database:
+        If the user is already saved return first instance of the user
+        else return None
+        :return:
+        """
+        df_json = pd.read_csv(self.authentication_csv_file_location)
+        user_dict_list = df_json.to_dict('records')
+        for user_data in user_dict_list:
+            if user_data.get('username') == self.username:
+                return user_data
+        return None
+
+    def save(self):
+        """
+        This function saves a new user to the database
+        :return:
+        """
+        user = self.check_if_user_is_already_saved()
+        if not user:
+            with open(self.authentication_csv_file_location, 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([self.username, self.password])
+            return True, user
+        else:
+            return False, user
+
+    def login(self):
+        """
+        This function logs in a user
+        :return:
+        """
+        self.is_authenticated = True
